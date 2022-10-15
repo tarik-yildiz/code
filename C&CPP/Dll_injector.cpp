ee093fbb45481153f7a1bd0d489f43d8a145eb4d
tarik-yildiz <112180001+tarik-yildiz@users.noreply.github.com>
2022-10-15 22:37:53 +0300

    Add files via upload

---
+++ C&CPP/Dll_injector.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <Windows.h>
+#include <string>
+
+#include <iostream>
+using namespace std;
+int main() {
+
+	string dllpath = "C:\\Users\\YOUR_PATH\\Dll1.dll";
+	DWORD procId = 18828;// DLL ENJEKTE EDILECEK PROCESS'IN ID'SI
+	HANDLE handle = OpenProcess(PROCESS_ALL_ACCESS, false, procId);
+	LPVOID LoadLibrary = (LPVOID)GetProcAddress(GetModuleHandle((LPCWSTR)"Kernel32.dll"), "LoadLibraryA");
+	LPVOID Memory = (LPVOID)VirtualAllocEx(handle, NULL, dllpath.length() + 1, MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
+	WriteProcessMemory(handle, (LPVOID)Memory, dllpath.c_str(), dllpath.length() + 1, NULL);
+	HANDLE RemoteThread = CreateRemoteThread(handle, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibrary, (LPVOID)Memory, NULL, NULL);
+	if (RemoteThread == NULL) {
+		cout << "HATA(" << GetLastError() << ")" << endl;
+	}
+	else {
+		cout << "BASARILI" << endl;
+	}
+
+	//hafizayi serbest birakiyoruz ve handle'i kapatiyoruz
+	VirtualFreeEx(handle, (LPVOID)Memory, 0, MEM_RELEASE);
+	CloseHandle(handle);
+	
+
+	return 1;
+}
